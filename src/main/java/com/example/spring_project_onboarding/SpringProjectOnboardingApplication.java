979824e97a4ea0690414ac98a63dc771e0be59db
Hi-Imjaeyoung <imjy2757@naver.com>
2024-02-02 11:59:35 +0900

    init project

---
+++ src/main/java/com/example/spring_project_onboarding/SpringProjectOnboardingApplication.java
@@ -0,0 +1,13 @@
+package com.example.spring_project_onboarding;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringProjectOnboardingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringProjectOnboardingApplication.class, args);
+	}
+
+}
